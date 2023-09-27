cbeedd32779722e73eb6682746988a5b5064e56d
sunah <1004_sunah@naver.com>
2023-09-28 07:21:31 +0900

    로직변경

--- js/commentList.js
+++ js/commentList.js
@@ -2,7 +2,7 @@
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { getDocs, getDoc, deleteDoc, addDoc, collection, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDocs, getDoc, deleteDoc, collection, doc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
@@ -28,12 +28,9 @@ const main = document.querySelector("main");
 // const savedTodos=localStorage.getItem(Todos_Key)
 let docs = await getDocs(collection(db, "comment"));
 
-let comments = [];
-
 //리스트 출력
 docs.forEach((doc) => {
     let row = doc.data();
-
     const li = document.createElement("li")
     li.id = doc.id;
     const span = document.createElement("span")
@@ -51,97 +48,22 @@ docs.forEach((doc) => {
     commentList.appendChild(li)
 })
 
-async function viewComment(event) {
+function viewComment(event) {
     const li = event.target.parentElement;
-    const docSnap = await getDoc(doc(db, "comment", li.id));
-    main.innerHTML = ""
-    main.innerHTML = `        
-        <div class="main-col">
-        <div class="reset">
-            <div class="reset__col">
-                <span>Wanna reset?</span>
-            </div>
-            <div class="reset__col">
-                <span><i class="fa-solid fa-arrow-rotate-right"></i></span>
-            </div>
-        </div>
-        <div class="comment-box">
-            <form id="comment_input">
-                <h2>[방명록] 칭찬해주세요</h2>
-                <div>
-                    <label for="floatingInput">제목</label>
-                    <input id="commentTitle" type="text" placeholder="제목을 입력해주세요"></input>
-                </div>
-                <div class="form-floating mb-3">
-                    <label for="floatingInput">추천 이유</label>
-                    <textarea class="form-control" id="comment" placeholder="추천 이유"></textarea>
-                </div>
-            </form>
-        </div>
-        </div>`
-    const docSnapData = docSnap.data();
-    const postingbtn = document.querySelector("#postingbtn");
-    document.querySelector("#commentTitle").value = docSnapData["commentTitle"];
-    document.querySelector("#comment").value = docSnapData["comment"];
-
+    location.href = "commentNew.html?id="+li.id+"&status=view"
 }
 async function editComment(event) {
-
-    const li = event.target.parentElement;
-    const docSnap = await getDoc(doc(db, "comment", li.id));
+    
+    const docSnap = await getDoc(doc(db, "comment", event.target.parentElement.id));
     let confirmPw = prompt("게시글의 수정을 위해 비밀번호를 입력해주세요");
-
     if (confirmPw == docSnap.data()['password']) {
-        main.innerHTML = ""
-        main.innerHTML = `        
-        <div class="main-col">
-        <div class="reset">
-            <div class="reset__col">
-                <span>Wanna reset?</span>
-            </div>
-            <div class="reset__col">
-                <span><i class="fa-solid fa-arrow-rotate-right"></i></span>
-            </div>
-        </div>
-        <div class="comment-box">
-            <form id="comment_input">
-                <h2>[방명록] 칭찬해주세요</h2>
-                <div>
-                    <label for="floatingInput">제목</label>
-                    <input id="commentTitle" type="text" placeholder="제목을 입력해주세요"></input>
-                </div>
-                <div class="form-floating mb-3">
-                    <label for="floatingInput">추천 이유</label>
-                    <textarea class="form-control" id="comment" placeholder="추천 이유"></textarea>
-                </div>
-            </form>
-            <button id="postingbtn" type="button" class="btn btn-danger">수정하기</button>
-        </div>
-        </div>`
-        const docSnapData = docSnap.data();
-        const postingbtn = document.querySelector("#postingbtn");
-        postingbtn.addEventListener("click", editCommentbtn);
-        document.querySelector("#commentTitle").value = docSnapData["commentTitle"];
-        document.querySelector("#comment").value = docSnapData["comment"];
-
-        async function editCommentbtn(event) {
-            let commentTitle = document.querySelector("#commentTitle").value
-            let comment = document.querySelector("#comment").value
-            await updateDoc(doc(db, "comment", li.id), {
-                'commentTitle': commentTitle,
-                'comment': comment,
-            });
-            alert('수정 완료!');
-            location.href = 'index.html';
-        }
-    } else {
+    location.href = "commentNew.html?id="+event.target.parentElement.id+"&status=edit"
+    }else {
         alert('올바르지 못한 비밀번호 입니다!');
     }
 }
 async function deleteComment(event) {
-
-    const li = event.target.parentElement;
-    const docSnap = await getDoc(doc(db, "comment", li.id));
+    const docSnap = await getDoc(doc(db, "comment", event.target.parentElement.id));
     let confirmPw = prompt("게시글의 삭제를 위해 비밀번호를 입력해주세요");
     if (confirmPw == docSnap.data()['password']) {
         await deleteDoc(doc(db, "comment", li.id));
--- js/commentNew.js
+++ js/commentNew.js
@@ -1,9 +1,8 @@
-
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+// import { collection } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDocs, getDoc, deleteDoc, addDoc, collection, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
@@ -22,24 +21,56 @@ const firebaseConfig = {
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const urlParams = new URL(location.href).searchParams;
+const status = urlParams.get('status');
+const commentId = urlParams.get('id');
+const postingbtn = document.getElementById('postingbtn');
 
-
-$("#postingbtn").click(async function () {
+if (status == "new") {
+    postingbtn.innerText = "기록하기";
+    postingbtn.addEventListener("click", createComment)
+} else if (status == "edit") {
+    postingbtn.innerText = "수정하기";
+    const docSnap = await getDoc(doc(db, "comment", commentId));
+    const docSnapData = docSnap.data();
+    document.querySelector("#commentTitle").value = docSnapData["commentTitle"];
+    document.querySelector("#comment").value = docSnapData["comment"];
+    postingbtn.addEventListener("click", editComment)
+} else {
+    postingbtn.remove();
+    const docSnap = await getDoc(doc(db, "comment", commentId));
+    const docSnapData = docSnap.data();
+    document.querySelector("#commentTitle").value = docSnapData["commentTitle"];
+    document.querySelector("#comment").value = docSnapData["comment"];
+}
+//생성
+async function createComment() {
     let commentTitle = $('#commentTitle').val();
     let comment = $('#comment').val();
     let password = prompt("게시글의 비밀번호를 입력해주세요");
-
-    let doc = {
-        'userName': localStorage.getItem('userName') ?localStorage.getItem('userName') :"익명",
+    if (password != null) {
+        let doc = {
+            'userName': localStorage.getItem('userName') ? localStorage.getItem('userName') : "익명",
+            'commentTitle': commentTitle,
+            'comment': comment,
+            'password': password,
+            'date': Date.now(),
+        }
+        await addDoc(collection(db, "comment"), doc);
+        alert('저장 완료!');
+        location.href = 'index.html';
+    } else {
+        alert('게시글 작성을 위해서는 비밀번호가 필요합니다!');
+    }
+}
+//수정
+async function editComment() {
+    let commentTitle = document.querySelector("#commentTitle").value
+    let comment = document.querySelector("#comment").value
+    await updateDoc(doc(db, "comment", commentId), {
         'commentTitle': commentTitle,
         'comment': comment,
-        'password': password,
-        'date': Date.now(),
-    }
-
-
-    console.log(doc);
-    await addDoc(collection(db, "comment"), doc);
-    alert('저장 완료!');
-    location.href='index.html';
-})
+    });
+    alert('수정 완료!');
+    location.href = 'index.html';
+}
