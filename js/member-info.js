284ab593ff0140e71a6023dc9681c586e0f16d7c
sunah <1004_sunah@naver.com>
2023-09-29 01:50:39 +0900

    member_info & team_info

--- js/member-info.js
+++ js/member-info.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
   const $membersInfoBox = $(".members-info-box");
   const $members = $(".member");
   const $teamInfoBox = $(".team-info-box");
+  const $welcome = $("#welcome");
+  const $mainBoxTodolist = $(".main-box__todolist");
 
   // 페이지 로드 시 기본적으로 숨기기
+  $welcome.show();
+  $mainBoxTodolist.hide();
   $members.hide();
   $membersInfoBox.hide();
   $teamInfoBox.hide();
+
+  // 5초 뒤에 welcome 문구 사라지게 만듦
+  setTimeout(function () {
+    $welcome.hide();
+    $mainBoxTodolist.show();
+  }, 5000);
 
   $("#team-button-box a").on("click", function (e) {
     e.preventDefault();
@@ -42,6 +52,19 @@ $(document).ready(function () {
     e.preventDefault();
     $members.hide();
     $membersInfoBox.show();
+  });
+
+  // members 링크 클릭 시 해당 멤버의 infobox만 보이게 함
+  $members.on("click", function (e) {
+    e.preventDefault();
+
+    // 모든 멤버 정보 박스 숨기고, 클릭한 멤버에 해당하는 정보 박스만 보이게 함
+    const memberId = $(this).attr("href");
+    // 클릭한 멤버의 href 속성 값 가져오기
+    $membersInfoBox.hide();
+    // 모든 멤버 정보 박스 숨기기
+    $(`${memberId}-info`).show();
+    // 해당하는 멤버 정보 박스 보이게 함
   });
 
   // 리셋 누를 시 초기화면으로 되돌림
