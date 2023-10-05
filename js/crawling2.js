ed16cf214eb646b7998b41c84c3bce11214abcf5
sunah <1004_sunah@naver.com>
2023-10-06 01:49:18 +0900

    병합중

--- js/crawling2.js
+++ js/crawling2.js
@@ -1,88 +1,44 @@
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDoc, doc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCauRZ02WOgnWSXDX7pEVv9xJ-g25bOyWE",
-    authDomain: "sparta5-65934.firebaseapp.com",
-    databaseURL: "https://sparta5-65934-default-rtdb.firebaseio.com",
-    projectId: "sparta5-65934",
-    storageBucket: "sparta5-65934.appspot.com",
-    messagingSenderId: "381298859705",
-    appId: "1:381298859705:web:b65a54d74b3b7f765b8568",
-    // measurementId: "G-PE2KPSE1FQ"
+  apiKey: "AIzaSyCauRZ02WOgnWSXDX7pEVv9xJ-g25bOyWE",
+  authDomain: "sparta5-65934.firebaseapp.com",
+  databaseURL: "https://sparta5-65934-default-rtdb.firebaseio.com",
+  projectId: "sparta5-65934",
+  storageBucket: "sparta5-65934.appspot.com",
+  messagingSenderId: "381298859705",
+  appId: "1:381298859705:web:b65a54d74b3b7f765b8568",
+  // measurementId: "G-PE2KPSE1FQ"
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const database = firebase.database();
 
-// 선아 데이터
-for(let i =1;i<=3;i++){
-    database.ref(`선아/${i}`).on("value", snapshot => {
-        const data = snapshot.val();
-        const {날짜:date, 내용: contents, 링크: links, 제목: title} = data;
-        lastIndex = contents.indexOf('.')+1;
-        document.querySelector(`.sunah${i}`).innerHTML =
-            `제목 : ${title}<br>
-            내용 : ${contents.slice(0,lastIndex)} <br>
-            날짜 : ${date} <br>
-            링크 : ${links}`;
-    });
-}
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-
-//지원 데이터
-for(let i =1;i<=3;i++){
-    database.ref(`지원/${i}`).on("value", snapshot => {
-        const data = snapshot.val();
-        const {날짜:date, 내용: contents, 링크: links, 제목: title} = data;
-        lastIndex = contents.indexOf('.')+1;
-        document.querySelector(`.jiwon${i}`).innerHTML =
-            `제목 : ${title}<br>
-            내용 : ${contents.slice(0,lastIndex)} <br>
-            날짜 : ${date} <br>
-            링크 : ${links}`;
-            console.log(title);
-            console.log(contents);
-            console.log(date);
-            console.log(links);
-
-    });
-}
-
-//태영 데이터
-for(let i =1;i<=2;i++){
-    database.ref(`태영/${i}`).on("value", snapshot => {
-        const data = snapshot.val();
-        const {날짜:date, 내용: contents, 링크: links, 제목: title} = data;
-        document.querySelector(`.taeyoung${i}`).innerHTML =
-            `제목 : ${title}<br>
-            내용 : ${contents} <br>
-            날짜 : ${date} <br>
-            링크 : ${links}`;
-    });
-}
-
-//유덕 데이터
-for(let i =1;i<=3;i++){
-    database.ref(`유덕/${i}`).on("value", snapshot => {
-        const data = snapshot.val();
-        const {날짜:date, 내용: contents, 링크: links, 제목: title} = data;
-        lastIndex = contents.indexOf('.')+1;
-        document.querySelector(`.yoodeock${i}`).innerHTML =
-            `제목 : ${title}<br>
-            내용 : ${contents.slice(0,lastIndex)} <br>
-            날짜 : ${date} <br>
-            링크 : ${links}`;
-    });
-}
-
-//윤찬 데이터
-for(let i =1;i<=3;i++){
-    database.ref(`윤찬/${i}`).on("value", snapshot => {
-        const data = snapshot.val();
-        const {날짜:date, 내용: contents, 링크: links, 제목: title} = data;
-        document.querySelector(`.yoonchan${i}`).innerHTML =
-            `제목 : ${title}<br>
-            내용 : ${contents} <br>
-            날짜 : ${date} <br>
-            링크 : ${links}`;
-    });
+async function fetchData(name, i) {
+  database.ref(`${name}/${i}`).on("value", (snapshot) => {
+    const data = snapshot.val();
+    const {
+      날짜: date,
+      링크: links,
+      제목: title,
+    } = data;
+    $(`.container${i}`).html(`
+      제목 : ${title}<br>
+      날짜 : ${date}
+    `);
+    $(`.container${i}`).wrap(`<div class = "memberContainer${i}"></div>`);
+    $(`.memberContainer${i}`).click(function(){
+		$(location).attr("href", `${links}`)
+	});
+  });
+  const docSnap = await getDoc(doc(db, "member", `member${i}`));
+  $(".social_btn").html(`
+  <img class ="github" href="${docSnap.data()['github']}" src="img/icon/25231.png"> </img>
+  <img class ="tistory" href="#" src="img/icon/270D883555ED252B1B.png"> </img>`)
 }
