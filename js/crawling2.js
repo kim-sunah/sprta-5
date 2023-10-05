6f2e3d4ef883425bddf012e51d6c033e4a6f71d0
sunah <1004_sunah@naver.com>
2023-10-06 02:07:59 +0900

    아이콘 변경

--- js/crawling2.js
+++ js/crawling2.js
@@ -1,8 +1,4 @@
 // Firebase SDK 라이브러리 가져오기
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { getDoc, doc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCauRZ02WOgnWSXDX7pEVv9xJ-g25bOyWE",
   authDomain: "sparta5-65934.firebaseapp.com",
@@ -17,10 +13,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-async function fetchData(name, i) {
+function fetchData(name, i) {
   database.ref(`${name}/${i}`).on("value", (snapshot) => {
     const data = snapshot.val();
     const {
@@ -37,8 +30,5 @@ async function fetchData(name, i) {
 		$(location).attr("href", `${links}`)
 	});
   });
-  const docSnap = await getDoc(doc(db, "member", `member${i}`));
-  $(".social_btn").html(`
-  <img class ="github" href="${docSnap.data()['github']}" src="img/icon/25231.png"> </img>
-  <img class ="tistory" href="#" src="img/icon/270D883555ED252B1B.png"> </img>`)
+  const docSnap = getDoc(doc(database, "member", `member${i}`));
 }
